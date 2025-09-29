adb1c2a286d1ae4877f2010efb8a5ee8e8927f4c
王钰滔 <879590811@qq.com>
2025-09-29 10:26:39 +0800

    feature-前端提交测试

--- kuncode-picture-frontend/src/api/typings.d.ts
+++ kuncode-picture-frontend/src/api/typings.d.ts
@@ -1,1 +1,154 @@
-declare namespace API {}
+declare namespace API {
+  type BaseResponseBoolean_ = {
+    code?: number
+    data?: boolean
+    message?: string
+  }
+
+  type BaseResponseLoginUserVo_ = {
+    code?: number
+    data?: LoginUserVo
+    message?: string
+  }
+
+  type BaseResponseLong_ = {
+    code?: number
+    data?: number
+    message?: string
+  }
+
+  type BaseResponsePageUserVO_ = {
+    code?: number
+    data?: PageUserVO_
+    message?: string
+  }
+
+  type BaseResponseUser_ = {
+    code?: number
+    data?: User
+    message?: string
+  }
+
+  type BaseResponseUserVO_ = {
+    code?: number
+    data?: UserVO
+    message?: string
+  }
+
+  type DeleteRequest = {
+    id?: number
+  }
+
+  type getUserByIdUsingGETParams = {
+    /** id */
+    id?: number
+  }
+
+  type getUserVOByIdUsingGETParams = {
+    /** id */
+    id?: number
+  }
+
+  type LoginUserVo = {
+    /** 创建时间 */
+    createTime?: string
+    /** 编辑时间 */
+    editTime?: string
+    /** id */
+    id?: number
+    /** 更新时间 */
+    updateTime?: string
+    /** 账号 */
+    userAccount?: string
+    /** 用户头像 */
+    userAvatar?: string
+    /** 用户昵称 */
+    userName?: string
+    /** 用户简介 */
+    userProfile?: string
+    /** 用户角色：user/admin */
+    userRole?: string
+  }
+
+  type PageUserVO_ = {
+    current?: number
+    pages?: number
+    records?: UserVO[]
+    size?: number
+    total?: number
+  }
+
+  type User = {
+    /** 创建时间 */
+    createTime?: string
+    /** 编辑时间 */
+    editTime?: string
+    /** id */
+    id?: number
+    /** 是否删除 */
+    isDelete?: number
+    /** 更新时间 */
+    updateTime?: string
+    /** 账号 */
+    userAccount?: string
+    /** 用户头像 */
+    userAvatar?: string
+    /** 用户昵称 */
+    userName?: string
+    /** 密码 */
+    userPassword?: string
+    /** 用户简介 */
+    userProfile?: string
+    /** 用户角色：user/admin */
+    userRole?: string
+  }
+
+  type UserAddRequest = {
+    userAccount?: string
+    userAvatar?: string
+    userName?: string
+    userProfile?: string
+    userRole?: string
+  }
+
+  type UserLoginRequest = {
+    userAccount?: string
+    userPassword?: string
+  }
+
+  type UserQueryRequest = {
+    id?: number
+    page?: number
+    pageSize?: number
+    sortField?: string
+    sortOrder?: string
+    userAccount?: string
+    userName?: string
+    userProfile?: string
+    userRole?: string
+  }
+
+  type UserRegisterRequest = {
+    checkPassword?: string
+    userAccount?: string
+    userPassword?: string
+  }
+
+  type UserUpdateRequest = {
+    id?: number
+    userAvatar?: string
+    userName?: string
+    userProfile?: string
+    userRole?: string
+  }
+
+  type UserVO = {
+    createTime?: string
+    id?: number
+    userAccount?: string
+    userAvatar?: string
+    userName?: string
+    userProfile?: string
+    userRole?: string
+  }
+}
